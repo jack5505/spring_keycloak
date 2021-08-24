731ebdb7e3262d1afee03dc46247509cf639df7b
Sabirov Jakhongir <jahon.080879@gmail.com>
2021-08-24 17:32:43 +0500

    Change role into users it was misspelling letter s and fix it . According to db of keycloak

--- src/main/java/com/example/keycloak/learnkeycloak/configuration/SecurityConfig.java
+++ src/main/java/com/example/keycloak/learnkeycloak/configuration/SecurityConfig.java
@@ -49,7 +49,7 @@ public class SecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
         super.configure(http);
         http
             .authorizeRequests()
-            .antMatchers("/employees*").hasRole("user")
+            .antMatchers("/employees*").hasRole("users")
             .anyRequest().permitAll();
     }
 
